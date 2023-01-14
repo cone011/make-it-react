6595b90f15a276f18655ff4ae2a21673e4591b35
Juan <jjcaimen@gmail.com>
2023-01-13 21:27:06 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { Fragment, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [incrementNumber, SetIncrementNumber] = useState(0);
+  const buttonHandler = () => {
+    SetIncrementNumber(incrementNumber++);
+  };
   return (
-    <div>
-      <h1>Mi primera aplicación</h1>
-      <p>Esta es mi primera aplicación en React, está padrísimo!</p>
-    </div>
+    <Fragment>
+      <h1>{incrementNumber}</h1>
+      <button onClick={buttonHandler}>Incrementa</button>
+    </Fragment>
+    // <div>
+    //   <h1>Mi primera aplicación</h1>
+    //   <p>Esta es mi primera aplicación en React, está padrísimo!</p>
+    // </div>
   );
 }
 
